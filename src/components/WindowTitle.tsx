9ae635890576c0643728df8d5899bf9b30bf0588
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-06-05 18:12:58 +0200

    ✨ feat(components): Add window title component

---
+++ src/components/WindowTitle.tsx
@@ -0,0 +1,14 @@
+export const Title = () => {
+
+  return (
+  <>
+   <div className="window-header">
+    <h1>Shopping List</h1>
+    <div className="window-buttons">
+      <button className="material-symbols-outlined minimize-btn">minimize</button>
+      <button className="material-symbols-outlined full-window-btn">web_asset</button>
+      <button className="material-symbols-outlined close-btn">close</button>
+    </div>
+   </div>
+  </>
+)}
