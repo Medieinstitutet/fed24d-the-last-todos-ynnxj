16b66e35ee379f5d3d6dbe9798291701f5fbbf70
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-06-06 16:43:11 +0200

    ✨ feat(components): Update window header w/ props

--- src/components/WindowTitle.tsx
+++ src/components/WindowTitle.tsx
@@ -1,14 +1,21 @@
-export const Title = () => {
+type TitleProps = {
+  mode: "list" | "add"
+}
 
+/**
+ * Render title header for windows.
+ * Different class depending on mode.
+ */
+export const Title =  ({ mode }: TitleProps) => {
   return (
   <>
-   <div className="window-header">
-    <h1>Shopping List</h1>
-    <div className="window-buttons">
-      <button className="material-symbols-outlined minimize-btn">minimize</button>
-      <button className="material-symbols-outlined full-window-btn">web_asset</button>
-      <button className="material-symbols-outlined close-btn">close</button>
+  <div className={mode === "list" ? "window-header" : "notepad-header"}>
+      {mode === "list" ? (<h1>Shopping List</h1>) : (<h2><img className="notepad-icon" src="src/assets/notepad.png"></img>Untitled - Notepad</h2>)}
+      <div className="window-buttons">
+        <button className="material-symbols-outlined minimize-btn">minimize</button>
+        <button className="material-symbols-outlined full-window-btn">web_asset</button>
+        <button className="material-symbols-outlined close-btn">close</button>
+      </div>
     </div>
-   </div>
   </>
 )}
