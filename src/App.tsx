c90a5ea7dd9641dba174e0e032c44e0e59388c1b
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-06-06 16:45:51 +0200

    ♻️ refactor(App.tsx): Update App.tsx
    Title is now in each component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.scss"
 import { ShoppingList } from "./components/Lists"
-import { Title } from "./components/WindowTitle"
 
 const App = () => {
   return (
     <>
       <section>
-        <Title/>
-        <ShoppingList/>
+        <ShoppingList />
       </section>
     </>
   )
